c57fa0577b855de2ebb2b03aba073d2fc039590d
Nicholas Pufal <npufal@thoughtworks.com>
2015-02-26 21:54:56 -0300

    feat(scheduled_credits) shows scheduled credits - date and amount

--- saldo.py
+++ saldo.py
@@ -13,22 +13,19 @@ import requests
 POST_URL = 'https://sodexosaldocartao.com.br/saldocartao/consultaSaldo.do?operation=consult'
 CAPTCHA_URL = 'https://sodexosaldocartao.com.br/saldocartao/jcaptcha.do'
 
-
 def parse_html(html):
     soup = BeautifulSoup(html)
+    parsed_amount = __parse_amount(soup)
+    parsed_schedule = __parse_schedule(soup)
 
-    result = soup.find(id='balance')
+    amount_output = 'Saldo: ' + parsed_amount
+    schedule_output = 'Proxima regarga: ' + parsed_schedule
 
-    if result:
-        extracted_amount = re.search('R\$\s[0-9\.]+', result.var.string)
-        return 'Seu saldo atual: ' + extracted_amount.group(0)
-
-    return 'Não foi possível verificar o saldo. Possíves razões: captcha inválido ou alteração no DOM da página'
+    return __decorate_output(amount_output, schedule_output)
 
 def get_captcha(session):
     r = session.get(CAPTCHA_URL)
     return Image.open(StringIO(r.content))
-
 
 def post_card(session, card_number, cpf, captcha_text):
     post_data = {
@@ -56,6 +53,28 @@ def get_balance(card_number, cpf):
 
     return parse_html(post_card(session, card_number, cpf, captcha_text))
 
+def __parse_amount(soup):
+    result = soup.find(id='balance')
+
+    if result:
+        extracted_amount = re.search('R\$\s[0-9\.]+', result.var.string)
+        return extracted_amount.group(0).decode('utf-8')
+
+    return 'Não foi possível verificar o saldo. Possíves razões: captcha inválido ou alteração no DOM da página'
+
+def __parse_schedule(soup):
+    result = soup.find('tr', class_='credito')
+
+    if result:
+        date = result.find('td')
+        amount = date.find_next('td')
+        return date.get_text() + ' no valor de R$ ' + amount.get_text()
+
+    return 'N/A'
+
+def __decorate_output(amount_output, schedule_output):
+    SEPARATOR = '\n-------------\n'
+    return SEPARATOR + amount_output + SEPARATOR + schedule_output + SEPARATOR
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
